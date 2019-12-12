f796a4eb584609725723ff43614ca6714e100acf
Lee <lee@benf.org>
2019-12-12 21:36:36 +0000

    more pi constants

---
+++ src_6/org/benf/cfr/tests/ConstantNameTest6.java
@@ -0,0 +1,64 @@
+package org.benf.cfr.tests;
+
+public strictfp class ConstantNameTest6 {
+    final double dpm10 = Math.PI * -10;
+    final double dpm9 = Math.PI * -9;
+    final double dpm8 = Math.PI * -8;
+    final double dpm7 = Math.PI * -7;
+    final double dpm6 = Math.PI * -6;
+    final double dpm5 = Math.PI * -5;
+    final double dpm4 = Math.PI * -4;
+    final double dpm3 = Math.PI * -3;
+    final double dpm2 = Math.PI * -2;
+    final double dpm1 = Math.PI * -1;
+    final double dp1 = Math.PI * 1;
+    final double dp2 = Math.PI * 2;
+    final double dp3 = Math.PI * 3;
+    final double dp4 = Math.PI * 4;
+    final double dp5 = Math.PI * 5;
+    final double dp6 = Math.PI * 6;
+    final double dp7 = Math.PI * 7;
+    final double dp8 = Math.PI * 8;
+    final double dp9 = Math.PI * 9;
+    final double dp10 = Math.PI * 10;
+
+    final double ddm90 = Math.PI / -90;
+    final double ddm180 = Math.PI / -180;
+    final double ddm270 = Math.PI / -270;
+    final double ddm360 = Math.PI / -360;
+    final double dd90 = Math.PI / 90;
+    final double dd180 = Math.PI / 180;
+    final double dd270 = Math.PI / 270;
+    final double dd360 = Math.PI / 360;
+
+    final float fpm10 = (float)Math.PI * -10;
+    final float fpm9 = (float)Math.PI * -9;
+    final float fpm8 = (float)Math.PI * -8;
+    final float fpm7 = (float)Math.PI * -7;
+    final float fpm6 = (float)Math.PI * -6;
+    final float fpm5 = (float)Math.PI * -5;
+    final float fpm4 = (float)Math.PI * -4;
+    final float fpm3 = (float)Math.PI * -3;
+    final float fpm2 = (float)Math.PI * -2;
+    final float fpm1 = (float)Math.PI * -1;
+    final float fp1 = (float)Math.PI * 1;
+    final float fp2 = (float)Math.PI * 2;
+    final float fp3 = (float)Math.PI * 3;
+    final float fp4 = (float)Math.PI * 4;
+    final float fp5 = (float)Math.PI * 5;
+    final float fp6 = (float)Math.PI * 6;
+    final float fp7 = (float)Math.PI * 7;
+    final float fp8 = (float)Math.PI * 8;
+    final float fp9 = (float)Math.PI * 9;
+    final float fp10 = (float)Math.PI * 10;
+
+    final float fdm90 = (float)Math.PI / -90;
+    final float fdm180 = (float)Math.PI / -180;
+    final float fdm270 = (float)Math.PI / -270;
+    final float fdm360 = (float)Math.PI / -360;
+    final float fd90 = (float)Math.PI / 90;
+    final float fd180 = (float)Math.PI / 180;
+    final float fd270 = (float)Math.PI / 270;
+    final float fd360 = (float)Math.PI / 360;
+
+}
---
+++ src_6/org/benf/cfr/tests/ConstantNameTest6a.java
@@ -0,0 +1,26 @@
+package org.benf.cfr.tests;
+
+public strictfp class ConstantNameTest6a {
+
+    final float fpm10 = (float)Math.PI * -10;
+    final float fpm7 = (float)Math.PI * -7;
+    final float fpm5 = (float)Math.PI * -5;
+    final float fp5 = (float)Math.PI * 5;
+    final float fp7 = (float)Math.PI * 7;
+    final float fp10 = (float)Math.PI * 10;
+    final float fpm10a = (float)(Math.PI * -10);
+    final float fpm7a = (float)(Math.PI * -7);
+    final float fpm5a = (float)(Math.PI * -5);
+    final float fp5a = (float)(Math.PI * 5);
+    final float fp7a = (float)(Math.PI * 7);
+    final float fp10a = (float)(Math.PI * 10);
+
+    final float fp2 = (float)Math.PI * 2;
+    final float fp2a = (float)(Math.PI * 2);
+
+    final float fdm270 = (float)Math.PI / -270;
+    final float fd270 = (float)Math.PI / 270;
+    final float fdm270a = (float)(Math.PI / -270);
+    final float fd270a = (float)(Math.PI / 270);
+
+}
