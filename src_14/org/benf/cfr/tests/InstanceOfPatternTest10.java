f4228fd6382a45ed8b2fb36291117d2de91d6afc
Lee <lee@benf.org>
2020-02-25 12:35:09 +0000

    comment

--- src_14/org/benf/cfr/tests/InstanceOfPatternTest10.java
+++ src_14/org/benf/cfr/tests/InstanceOfPatternTest10.java
@@ -1,6 +1,13 @@
 package org.benf.cfr.tests;
 
 // From a discussion with Tom Johnson
+/*
+ * In case you don't want to run, as of java (build 14-ea+34-1452)
+ * this prints:
+ *
+ * Fred
+ * WIBBLE
+ */
 public class InstanceOfPatternTest10 {
     static String s = "WIBBLE";
 
